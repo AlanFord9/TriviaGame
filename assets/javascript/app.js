548762db77b466024ea20f481c1dae5bd45b919a
frankcodes <alanforducf@gmail.com>
2020-01-21 17:01:46 -0500

    showing questions with answer options, added function for input

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,5 @@
-// creat click event to start timer and quiz
-
-
 var startButton = document.getElementById('start')
 var questions = document.getElementById('questions')
-var qAnswers = document.getElementById('answers')
 var submitButton = document.getElementById('submit')
 var clockRunning = false
 var clock = 30
@@ -55,7 +51,7 @@ var allQuestions = [
         correctAnswer: 2
     },
     {
-        question: "You son of a b****...",
+        question: "You son of a b****   ........",
         answers: [
             "I'm out",
             "You got me",
@@ -72,7 +68,7 @@ var allQuestions = [
         correctAnswer: 1
     },
     {
-        question: "Umm, okay last one. Just make like 5 three-pointers and we're good",
+        question: "Umm, okay last one. Just make like 5 three-pointers and we're good..",
         answers: [
             "1 three-pointer",
             "4 three-pointers",
@@ -88,10 +84,24 @@ $("#start").on("click", startQuiz)
 
 function startQuiz() {
 
+
     for(i = 0; i < allQuestions.length; i++) {
-        console.log(allQuestions[i]);
-            // assign questions/answers to respective html
-    };
+
+        $(questions).append("<h3>" + allQuestions[i].question + "</h3>");
+        
+        for(j = 0; j < allQuestions[i].answers.length; j++){
+
+            $(questions).append("<div class=form-check form-check-inline>" + 
+            "<input id=answer name=one class=form-check-input type=checkbox data-value="+[j]+">" +
+            "<label class=form-check-label>" + allQuestions[i].answers[j] + "</label>"
+            + "</div>");
+
+        }
+
+
+        // show questions and answer options
+
+    }
 
     var timerId = setInterval(countdown, 1000);
     function countdown(){
@@ -102,18 +112,32 @@ function startQuiz() {
             timer.innerHTML = 'Timer: ' + clock;
             clock--;
         }
-    }
-    
+        }
+
 }
 
 // creat logic for if the correct/incorrect answer was chosen
 
-// show ending results after timer finishes or "done" button is selected
-
-function showResults() {
-
-
+function userAnswer() {
+    if ($('input.form-check-input').prop('checked')){        
+        console.log($('input.form-check-input').val());
+    }
 }
 
-
 submitButton.addEventListener('click', showResults);
+
+function showResults() {
+    userAnswer()
+    alert("All Done!");
+    var correct = 0;
+    var wrong = 0;
+    var noAnswer = 0;
+
+    // change from text to html, add header inside
+    $("#congrats").text("Congrats, you finished or whatever..");
+    $("#correct").text("Correct Answers: " + correct);
+    $("#wrong").text("Incorrect Answers: " + wrong);
+    $("#unanswered").text("Unanswered: " + noAnswer);
+}
+
+// show results and end timer after button click
